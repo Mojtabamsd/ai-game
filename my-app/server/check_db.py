77b466f46513afe3a163c2ad0c645014be634715
mojmas <masoudi.m1991@gmail.com>
2026-06-09 16:52:05 +0100

    handle absolute or web-style path like

--- my-app/server/check_db.py
+++ my-app/server/check_db.py
@@ -1,6 +1,6 @@
 import sqlite3, pandas as pd
 
-con = sqlite3.connect("game.db")
+con = sqlite3.connect("../data/game.db")
 sessions   = pd.read_sql("SELECT * FROM sessions", con)
 selections = pd.read_sql("SELECT * FROM selections", con)
 
